fe14219f86c128bb79502f195f0c4fa85a1cd1f8
Daniel Balchev <dbalchev@gmail.com>
2024-11-02 11:59:50 +0200

    Use dev build for testing

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test'
 
 test('welcomes you', async ({ page }) => {
-    await page.goto('http://localhost:3000')
+    await page.goto('/')
 
     // Expect a title "to contain" a substring.
     await expect(page.locator('#welcome')).toContainText('Welcome to the bouldering app')
