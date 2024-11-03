2e18eb3968bd6ab45767e0efbdc0c64cc223c237
Daniel Balchev <dbalchev@gmail.com>
2024-11-03 21:45:37 +0200

    Override basepath

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
     output: 'export',
     distDir: 'build',
+    basePath: '/boulder-competition-app',
 }
 
 export default nextConfig
